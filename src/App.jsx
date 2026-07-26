8760ca1969742e305e0b0e95857f192bf910c29f
4zb82dw9dm-creator <4zb82dw9dm@privaterelay.appleid.com>
2026-07-26 14:09:04 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Nanbudo Competition</h1>
+      <p>Application de gestion des compétitions AFDP Nanbudo</p>
+    </div>
+  );
+}
+
+export default App;
