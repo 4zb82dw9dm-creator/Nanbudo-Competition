ffc63254639ab4854e4b7a8fce7580cb5ca27955
4zb82dw9dm-creator <4zb82dw9dm@privaterelay.appleid.com>
2026-07-27 09:18:57 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -119,7 +119,7 @@ function App() {
 
         {section === "competitions" && (
   <CompetitionManager />
-)}          <section>
+)}          
           
         {section === "organisateurs" && (
           <section>
