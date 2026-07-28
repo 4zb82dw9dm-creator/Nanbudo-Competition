e20918f3e118936e9dea357be4c95868cd7f232f
4zb82dw9dm-creator <4zb82dw9dm@privaterelay.appleid.com>
2026-07-28 13:01:36 +0200

    Update CompetitionDashboard.jsx

--- src/CompetitionDashboard.jsx
+++ src/CompetitionDashboard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import CategoriesManager from "./CategoriesManager";
 import PoolsManager from "./PoolsManager";
 import ArbitrationManager from "./ArbitrationManager";
